05ef103c9194d4e808a2be5ede3431af4b8be429
Alphatule <66392548+alphatule@users.noreply.github.com>
2024-03-06 18:39:10 +0100

    Ejercicios de recursividad

--- DMOJ/src/c7l21successiode.java
+++ DMOJ/src/c7l21successiode.java
@@ -1,26 +1,21 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class c7l21successiode {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
-
         int n = sc.nextInt();
         if (n < 2) System.out.println(n);
         else {
-
+            System.out.println(fib(n, 2, 0, 1));
         }
-        System.out.println(fib(n, 2, 0, 1));
-
     }
-
 
     // n es
     // i es
     // a es el numero penultimo de la secuencia en la que estamos
     // b es el numero ultimo de la secuencia en la que estamos
-
     private static int fib(int n, int i, int a, int b){
         if (n == i) return a+b;
         else return fib(n, i+1, b, a+b);
--- TESTExamen/src/Main.java
+++ TESTExamen/src/Main.java
@@ -6,40 +6,10 @@ public class Main{
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
 
-        int[] linea1 = new int[0];
-        int[] linea2 = new int[0];
-        int posicionNumero = 0;
-        for (int x = 0; x < 3; x++) {
-            if (x < 2) {
-                char[] lineaString = sc.nextLine().replaceAll(" ", "").toCharArray();
-                int[] lineaEnteros = new int[lineaString.length];
-                for (int i = 0; i < lineaString.length; i++) {
-
-                    lineaEnteros[i] = Integer.parseInt(String.valueOf(lineaString[i]));
-                }
-                if (x == 0) {
-                    linea1 = lineaEnteros;
-                } else if (x == 1) {
-                    linea2 = lineaEnteros;
-                }
-            } else {
-                // Aqui va el ultimo read del Scanner (lee el ultimo entero)
-                posicionNumero = sc.nextInt();
-            }
+        int n = sc.nextInt();
+        while (n < 10){
+            System.out.print(n);
         }
-        if ( ((linea1[posicionNumero] + linea2[posicionNumero]) % 2) == 0) {
-            System.out.println("La suma dels números en la posició " + posicionNumero + " retorna un nombre parell.");
-        } else {
-            System.out.println("La suma dels números en la posició " + posicionNumero + " retorna un nombre senar.");
-        }
-        boolean arraysIguales = true;
-        for (int i = 0; i < linea1.length; i++) {
-            if (linea1[i] != linea2[i]) {
-                arraysIguales = false;
-            }
-        }
-
-        System.out.println( ((arraysIguales) ? "Tots el números són els mateixos." : "Els números no són els mateixos.") );
 
     }
 
